2526b9ab6156680567d39bcd79d95cf5aedcff67
PS Narayanan <thepsnarayanan@gmail.com>
2020-06-02 14:55:24 +0530

    Templates and views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'base/main.html')
+
+def  profile(request):
+    return HttpResponse('profile')
+    
