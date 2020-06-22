958811919dc20805c9390ccf6966632eaf4bbcfc
PS Narayanan <thepsnarayanan@gmail.com>
2020-06-22 21:57:44 +0530

    moving static files to one dir

--- bhaavi/settings.py
+++ bhaavi/settings.py
@@ -131,8 +131,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[ 
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'accounts/static'),
-    os.path.join(BASE_DIR, 'agora/static'),
 ]
 #STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
