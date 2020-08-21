6904f7c2db01bb791a0625d7354840f72139b82c
iamharshkumar <hk7206210@gmail.com>
2020-08-21 15:11:07 +0530

    add purchase product when 100% 0ff

--- payment/urls.py
+++ payment/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('payment/initiate/<id>', views.createOrder, name="payment"),
     path('payment/status', views.paymentStatus, name='payment_status'),
     path('payment/done', views.paymentSuccessPage, name="success"),
+    path('payment/free', views.freePurchasePage, name="free-success"),
     path('payment/coupon', views.add_coupon, name="coupon"),
     path('payment/removeCoupon', views.remove_coupon, name="removeCoupon"),
 ]
