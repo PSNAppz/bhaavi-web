e6238f4129784cff3bcf0da1742e7eb70baf304e
PS Narayanan <thepsnarayanan@gmail.com>
2020-10-24 13:29:35 +0530

    add wagtail to static

--- bhaavi/settings.py
+++ bhaavi/settings.py
@@ -161,11 +161,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
     messages.INFO: 'alert-info',
